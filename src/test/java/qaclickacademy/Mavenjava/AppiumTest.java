885dc2cba5843e88850f88ffea3438b45bdb7ef5
fayaz0062 <fayaz0062@gmail.com>
2022-10-20 15:08:52 +0530

    Fayaz's First commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,30 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndroid()
+	{
+		
+		System.out.println("Android Apps");
+	}
+	
+	@Test
+	public void NativeApiumAndroid()
+	{
+		
+		System.out.println("NativeApiumAndroid Apps1");
+		System.out.println("NativeApiumAndroid Apps2");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOS Apps1");
+		System.out.println("IOS Apps2");
+		System.out.println("IOS Apps3");
+		System.out.println("IOS Apps4");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,24 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		
+		System.out.println(" Jira Apps automation");
+
+		System.out.println(" Jira2 Apps automation");
+		System.out.println(" Jira3 Apps automation");
+
+	}
+	
+	//@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleting twitter App");
+		System.out.println("Adding  twitter App");
+	}
+}
